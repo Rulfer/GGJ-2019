955d806618e55d58aeff4b550fe560687d13a368
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2019-01-25 21:13:35 +0100

    House scripts that reduces angular drag over time

---
+++ GGJ19/Assets/Scripts/House.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    GameObject house;
+    [SerializeField] private float startAngularDrag = 100;
+    [SerializeField] private float angularDragChangeRate = 10.0f;
+    float timer;
+ 
+    void Start()
+    {
+        house = FindObjectOfType<House>().gameObject;
+        house.GetComponent<Rigidbody2D>().angularDrag = startAngularDrag;
+        timer = 0.0f;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer % 60 >= angularDragChangeRate)
+        {
+            house.GetComponent<Rigidbody2D>().angularDrag /= 2;
+            timer = 0.0f;
+        }
+    }
+}
