8f8f4a00ff3474fa664d2ad99a5a2631e382efc2
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2019-01-25 21:12:36 +0100

    Count number of elements leaving/entered

--- GGJ19/Assets/Scripts/Side.cs
+++ GGJ19/Assets/Scripts/Side.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class Side : MonoBehaviour
 {
     HashSet<GameObject> elementsInside = new HashSet<GameObject>();
+    public int enterCounter;
+    public int leavecounter;
+    GameObject side;
 
     void Start()
     {
-        
+        side = FindObjectOfType<Side>().gameObject;
+        enterCounter = 0;
+        leavecounter = 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Throwable")
+        if (collision.gameObject.tag == "Throwable") {
+            enterCounter += 1;
             elementsInside.Add(collision.gameObject);
+        }
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Throwable")
+        {
+            leavecounter += 1;
             elementsInside.Remove(collision.gameObject);
+        }
     }
 }
