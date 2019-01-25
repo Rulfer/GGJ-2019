abe674b1ca5db2f4be6dfaed7d33a10a31e76f8a
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2019-01-25 20:20:38 +0100

    Basic script for tracking elements on each side

---
+++ GGJ19/Assets/Scripts/Side.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Side : MonoBehaviour
+{
+    HashSet<GameObject> elementsInside = new HashSet<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Throwable")
+            elementsInside.Add(collision.gameObject);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Throwable")
+            elementsInside.Remove(collision.gameObject);
+    }
+}
