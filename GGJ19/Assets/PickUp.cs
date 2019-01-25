9cb367defdbfb71e2f9a7842e658c1cd35e40675
Bard H. R <br@makingview.com>
2019-01-25 22:18:41 +0100

    a

---
+++ GGJ19/Assets/PickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField] private GameObject img_can_pick_up = null;
+
+
+    private List<GameObject> objects_to_pick_up = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision hit");
+
+        if(collision.transform.tag.Equals(""))
+    }
+}
