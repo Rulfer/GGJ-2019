bc2299878cdcea42956d32b49aa69b1fc8a46574
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2019-01-26 13:26:24 +0100

    House.cs: Declare a victor if House angle is above 20 degrees to either side

--- GGJ19/Assets/Scripts/House.cs
+++ GGJ19/Assets/Scripts/House.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class House : MonoBehaviour
 {
     GameObject house;
     private float startAngularDrag = 100;
     private float angularDragChangeRate = 15.0f;
+    private float victoryAngle = 20.0f;
     float timer;
  
     void Start()
@@ -21,6 +23,27 @@ public class House : MonoBehaviour
         {
             house.GetComponent<Rigidbody2D>().angularDrag /= 2;
             timer = 0.0f;
+        }
+        CheckGameEnd();
+    }
+
+    private void CheckGameEnd()
+    {
+        float zAngle = house.transform.eulerAngles.z;
+        if (zAngle >= victoryAngle && zAngle <= 360.0f - victoryAngle)
+        {
+            if (zAngle <= 180.0f)
+            {
+                Debug.Log("Right side wins!");
+            }
+            else if (zAngle > 180.0f)
+            {
+                Debug.Log("Left side wins!");
+            }
+        }
+        if (house.transform.position.y < -20)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
