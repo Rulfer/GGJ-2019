0650c82db87689156133dcf88ccceffa8a0d2deb
Bard H. R <br@makingview.com>
2019-01-26 13:14:50 +0100

    Added lights that tilt with the cabin

---
+++ GGJ19/Assets/Lamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    Transform house;
+
+    private void Start()
+    {
+        house = this.transform.parent;
+    }
+
+    private void Update()
+    {
+        //this.transform.rotation = Quaternion.Euler(0, 0, -house.transform.rotation.eulerAngles.z);
+        this.transform.eulerAngles = new Vector3(0, 0, -house.transform.rotation.z);
+    }
+}
